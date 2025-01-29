6564b3301edeb80c73d237632c39fd524645194d
tea why <funkiin.forever@gmail.com>
2025-01-29 09:17:49 -0500

    Update PauseSubstate.hx

--- source/rain/substates/PauseSubstate.hx
+++ source/rain/substates/PauseSubstate.hx
@@ -16,6 +16,8 @@ class PauseSubstate extends RainSubstate
 	private var currentSelection:Int = 0;
 	private var pauseOverlay:FlxSprite;
 	private var titleText:FlxText;
+	private var songText:FlxText;
+	private var difficultyText:FlxText;
 	private var selector:FlxSprite;
 
 	public function new()
@@ -25,28 +27,51 @@ class PauseSubstate extends RainSubstate
 		pauseOverlay = new FlxSprite().makeGraphic(FlxG.width, FlxG.height, FlxColor.BLACK);
 		pauseOverlay.alpha = 0;
 		add(pauseOverlay);
-		FlxTween.tween(pauseOverlay, {alpha: 0.5}, 0.2);
+		FlxTween.tween(pauseOverlay, {alpha: 0.6}, 0.2);
 
-		titleText = new FlxText(0, 50, FlxG.width, "PAUSED");
-		titleText.setFormat("assets/fonts/vcr.ttf", 64, FlxColor.WHITE, CENTER);
+		titleText = new FlxText(0, 40, FlxG.width, "PAUSED");
+		titleText.setFormat("assets/fonts/vcr.ttf", 64, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		titleText.borderSize = 2;
 		titleText.alpha = 0;
 		titleText.y -= 20;
 		add(titleText);
 		FlxTween.tween(titleText, {alpha: 1, y: titleText.y + 20}, 0.3, {startDelay: 0.1});
+
+		songText = new FlxText(0, 120, FlxG.width, '${PlayState.SONG.song}');
+		songText.setFormat("assets/fonts/vcr.ttf", 36, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		songText.borderSize = 1.5;
+		songText.alpha = 0;
+		songText.y -= 20;
+		add(songText);
+		FlxTween.tween(songText, {alpha: 1, y: songText.y + 20}, 0.3, {startDelay: 0.2});
+
+		difficultyText = new FlxText(0, 160, FlxG.width, '< ${PlayState.storyDifficulty == 0 ? "EASY" : PlayState.storyDifficulty == 1 ? "NORMAL" : "HARD"} >');
+		difficultyText.setFormat("assets/fonts/vcr.ttf", 24, FlxColor.GRAY, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		difficultyText.borderSize = 1;
+		difficultyText.alpha = 0;
+		difficultyText.y -= 20;
+		add(difficultyText);
+		FlxTween.tween(difficultyText, {alpha: 1, y: difficultyText.y + 20}, 0.3, {startDelay: 0.3});
 
 		optionTexts = new FlxTypedGroup<FlxText>();
 		add(optionTexts);
 
 		for (i in 0...options.length)
 		{
-			var optionText = new FlxText(0, 200 + (i * 60), FlxG.width, options[i]);
-			optionText.setFormat(null, 32, FlxColor.WHITE, CENTER);
+			var optionText = new FlxText(0, 250 + (i * 60), FlxG.width, options[i]);
+			optionText.setFormat("assets/fonts/vcr.ttf", 32, FlxColor.WHITE, CENTER, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+			optionText.borderSize = 1;
 			optionText.ID = i;
+			optionText.alpha = 0;
+			optionText.y -= 20;
 			optionTexts.add(optionText);
+			FlxTween.tween(optionText, {alpha: 1, y: optionText.y + 20}, 0.3, {startDelay: 0.3 + (i * 0.1)});
 		}
 
-		selector = new FlxSprite().makeGraphic(10, 30, FlxColor.WHITE);
+		selector = new FlxSprite().makeGraphic(20, 30, FlxColor.WHITE);
+		selector.alpha = 0;
 		add(selector);
+		FlxTween.tween(selector, {alpha: 1}, 0.3, {startDelay: 0.6});
 
 		updateSelection();
 	}
